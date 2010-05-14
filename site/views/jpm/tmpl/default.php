c4ffb6dd99043869e1711aabe69393afbcd568c6
Aşkın Yollu <askin@askin.ws>
2010-05-14 20:15:59 +0300

    use generic uri

--- site/views/jpm/tmpl/default.php
+++ site/views/jpm/tmpl/default.php
@@ -3,23 +3,27 @@
 // No direct access
 
 defined('_JEXEC') or die('Restricted access'); ?>
+<?php
+    jimport( 'joomla.environment.uri' );
+    $base_url = JURI::base();
+?>
 
 <div id="dhtmlgoodies_slideshow">
   <div id="previewPane">
-    <img src="http://localhost/siteler/joomla/<?php echo $this->items[0]->pic_big ?>" />
+    <img src="<?php echo $base_url; ?><?php echo $this->items[0]->pic_big ?>" />
     <span id="waitMessage">Resim yükleniyor. Lütfen bekleyin</span>
     <div id="largeImageCaption">
       <a href="<?php echo $this->items[1]->page_url ?>"><?php echo $this->items[0]->page_title ?>, detaylar için tıklayınız..</a>
     </div>
   </div>
   <div id="galleryContainer">
-    <div id="arrow_left"><img src="http://localhost/siteler/joomla/images/arrow_left.gif"></div>
-    <div id="arrow_right"><img src="http://localhost/siteler/joomla/images/arrow_right.gif"></div>
+    <div id="arrow_left"><img src="<?php echo $base_url; ?>components/com_jpm/images/arrow_left.gif" /></div>
+    <div id="arrow_right"><img src="<?php echo $base_url; ?>components/com_jpm/images/arrow_right.gif"/></div>
     <div id="theImages">
       <!-- Thumbnails -->
       <?php for ($i=0, $n=count( $this->items ); $i < $n; $i++) { ?>
-      <a href="#" onclick="showPreview('http://localhost/siteler/joomla/<?php echo $this->items[$i]->pic_big ?>','<?php echo $this->items[$i]->ID ?>');return false">
-        <img src="http://localhost/siteler/joomla/<?php echo $this->items[$i]->pic_small ?>">
+      <a href="#" onclick="showPreview('<?php echo $this->items[$i]->pic_big ?>','<?php echo $this->items[$i]->ID ?>');return false">
+        <img src="<?php echo $base_url; ?><?php echo $this->items[$i]->pic_small ?>">
       </a>
       <?php } ?>
       <!-- Image captions -->
